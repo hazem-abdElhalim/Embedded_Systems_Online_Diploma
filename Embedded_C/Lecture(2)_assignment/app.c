7e6c73e7faa22efbaafa4774c5b9a38a1f7a3f31
hazem <hazem@gmail.com>
2023-09-10 20:22:11 +0300

    Lesson(2)_assignment

--- Embedded_C/Lecture(2)_assignment/app.c
+++ Embedded_C/Lecture(2)_assignment/app.c
@@ -1,7 +1,7 @@
 
 #include "Uart.h"
 
-unsigned char string_uart[100]="learn-in-depth:<keroles>";
+unsigned char string_uart[100]="learn-in-depth:<Hazem>";
 void main(){
 	
 	Uart_u32GetString ( string_uart );
